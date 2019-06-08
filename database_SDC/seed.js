49c77d40aefe612959c797df936695e17f548cf9
Matt Violet <mattviolet@gmail.com>
2019-06-07 20:11:48 -0700

    Remove casual invocations from seeding for loops

--- database_SDC/seed.js
+++ database_SDC/seed.js
@@ -3,23 +3,57 @@ const csvWriter = require('csv-write-stream');
 var writer = csvWriter();
 const casual = require('casual');
 
-var numberOfHomes = 100000;
-var reviewsPerHome = 100;
 // var numberOfHomes = 10;
 // var reviewsPerHome = 3;
+var numberOfHomes = 100000;
+var reviewsPerHome = 100;
 var review_id = 1;
-var name = () => casual.first_name;
-var gender = () => casual.integer(0, 1);
-var profilepicnum = () => casual.integer(0, 99);
-var date = () => casual.date('MMMM YYYY');
-var sentence = () => casual.text; 
-var accuracy_rating = () => (Math.floor((Math.random() * 50)) / 10);
-var communication_rating = () => (Math.floor((Math.random() * 50)) / 10);
-var cleanliness_rating = () => (Math.floor((Math.random() * 50)) / 10);
-var location_rating = () => (Math.floor((Math.random() * 50)) / 10);
-var check_in_rating = () => (Math.floor((Math.random() * 50)) / 10);
-var value_rating = () => (Math.floor((Math.random() * 50)) / 10);
-var overall_rating = () => (Math.floor((Math.random() * 50)) / 10);
+var usernames = [];
+var profilePicNums = [];
+var dates = [];
+var sentences = [];
+
+var chooseRandom = (array) => {
+  var result = array[Math.floor(Math.random() * array.length)];
+  return result;
+}
+
+var generateNamesArr = (x) => {
+  for (var i = 0; i < x; i++) {
+    usernames.push(casual.first_name);
+  }
+}
+
+var chooseGender = () => {
+  var gender = Math.floor(Math.random() * 2);
+  return gender;
+}
+
+var generateProfilePicNumArr = (x) => {
+  for (var i = 0; i < x; i++) {
+    profilePicNums.push(casual.integer(0, 99));
+  }
+}
+
+var generateDates = (x) => {
+  for (var i = 0; i < x; i++) {
+    dates.push(casual.date('MMMM YYYY'));
+  }
+}
+
+var generateSentences = (x) => {
+  sentences.push(casual.text);
+}
+
+var generateRating = () => {
+  var rating = Math.floor((Math.random() * 50)) / 10;
+  return rating;
+}
+
+generateNamesArr(50);
+generateProfilePicNumArr(50);
+generateDates(50);
+generateSentences(50);
 
 
 const dataGen = () => {
@@ -31,18 +65,18 @@ const dataGen = () => {
       writer.write({
         review_id: review_id,
         room_id: i+1,
-        username: name(),
-        gender: gender(),
-        profilepicnum: profilepicnum(),
-        reviewdate: date(),
-        sentence: sentence(), 
-        accuracy_rating: accuracy_rating(),
-        communication_rating: communication_rating(),
-        cleanliness_rating: cleanliness_rating(),
-        location_rating: location_rating(),
-        check_in_rating: check_in_rating(),
-        value_rating: value_rating(),
-        overall_rating: overall_rating(),
+        username: chooseRandom(usernames),
+        gender: chooseGender(),
+        profilepicnum: chooseRandom(profilePicNums),
+        reviewdate: chooseRandom(dates),
+        sentence: chooseRandom(sentences), 
+        accuracy_rating: generateRating(),
+        communication_rating: generateRating(),
+        cleanliness_rating: generateRating(),
+        location_rating: generateRating(),
+        check_in_rating: generateRating(),
+        value_rating: generateRating(),
+        overall_rating: generateRating(),
       });
       review_id++;
     }
